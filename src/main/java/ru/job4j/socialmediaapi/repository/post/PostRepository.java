0a3d5d7fc6a07f4fad87b15d5d6ebc46a199f764
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2025-04-01 11:09:06 +0600

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmediaapi/repository/post/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/post/PostRepository.java
@@ -1,9 +1,20 @@
 package ru.job4j.socialmediaapi.repository.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.socialmediaapi.entity.Post;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface PostRepository extends ListCrudRepository<Post, Long> {
+
+    List<Post> findByUserId(Long userId);
+
+    List<Post> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
