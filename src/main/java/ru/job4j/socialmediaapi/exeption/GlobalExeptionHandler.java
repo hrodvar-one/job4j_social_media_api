894adacbfa4af42ff03aa6c2cf4081246e100714
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2025-04-08 13:17:24 +0600

    7. Обработка исключений в Spring REST [#505112]

--- src/main/java/ru/job4j/socialmediaapi/exeption/GlobalExeptionHandler.java
+++ src/main/java/ru/job4j/socialmediaapi/exeption/GlobalExeptionHandler.java
@@ -1,11 +1,16 @@
 package ru.job4j.socialmediaapi.exeption;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,5 +38,18 @@ public class GlobalExeptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleGeneral(Exception ex) {
         return ResponseEntity.internalServerError().body(Map.of("error", "Unexpected error: " + ex.getMessage()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public void catchDataIntegrityViolationException(Exception e, HttpServletRequest request, HttpServletResponse response)
+        throws Exception {
+        Map<String, String> details = new HashMap<>();
+        details.put("message", e.getMessage());
+        details.put("type", String.valueOf(e.getClass()));
+        details.put("timestamp", String.valueOf(LocalDateTime.now()));
+        details.put("path", request.getRequestURI());
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json; charset=utf-8");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(details));
     }
 }
